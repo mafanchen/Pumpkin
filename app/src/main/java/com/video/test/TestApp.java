c5751aded448d9d548db46d26a875f2cbe9b8de3
Reus <Reus@gmail.com>
2020-01-17 11:00:25 +0800

    修复后台切前台广告不显示的问题

--- app/src/main/java/com/video/test/TestApp.java
+++ app/src/main/java/com/video/test/TestApp.java
@@ -79,8 +79,6 @@ public class TestApp extends MultiDexApplication {
      */
     private long currentBackgroundTime;
 
-    private Handler handler = new Handler(Looper.getMainLooper());
-
     @Override
     public void onCreate() {
         super.onCreate();
@@ -523,30 +521,24 @@ public class TestApp extends MultiDexApplication {
                     isBackground = false;
                     new Thread(() -> {
                         SplashBean saveSplashBean = SpUtils.getSerializable(TestApp.getContext(), "splashBean");
-                        if (handler == null) {
-                            return;
-                        }
-                        handler.post(() -> {
-                            if (saveSplashBean != null) {
-                                int countTime = saveSplashBean.getCount_time();
-                                if (countTime == 0) {
-                                    countTime = AppConstant.TIME_PLAY_AD_WHEN_BACKGROUND;
-                                }
-                                if (System.currentTimeMillis() - currentBackgroundTime >= countTime * 1000 && isOpen) {
-                                    //这里判断是否有缓存
-                                    if (!TextUtils.isEmpty(saveSplashBean.getJump_url()) && !TextUtils.isEmpty(saveSplashBean.getPic_url())) {
-                                        ARouter.getInstance().build("/ad/activity")
-                                                .withString("ad_name", saveSplashBean.getAd_name())
-                                                .withString("jump_url", saveSplashBean.getJump_url())
-                                                .withString("pic_url", saveSplashBean.getPic_url())
-                                                .withString("ad_id", saveSplashBean.getId())
-                                                .withInt("showTime", saveSplashBean.getShow_time())
-                                                .navigation();
-                                    }
+                        if (saveSplashBean != null) {
+                            int countTime = saveSplashBean.getCount_time();
+                            if (countTime == 0) {
+                                countTime = AppConstant.TIME_PLAY_AD_WHEN_BACKGROUND;
+                            }
+                            if (System.currentTimeMillis() - currentBackgroundTime >= countTime * 1000 && isOpen) {
+                                //这里判断是否有缓存
+                                if (!TextUtils.isEmpty(saveSplashBean.getJump_url()) && !TextUtils.isEmpty(saveSplashBean.getPic_url())) {
+                                    ARouter.getInstance().build("/ad/activity")
+                                            .withString("ad_name", saveSplashBean.getAd_name())
+                                            .withString("jump_url", saveSplashBean.getJump_url())
+                                            .withString("pic_url", saveSplashBean.getPic_url())
+                                            .withString("ad_id", saveSplashBean.getId())
+                                            .withInt("showTime", saveSplashBean.getShow_time())
+                                            .navigation(activity);
                                 }
                             }
-
-                        });
+                        }
                     }).run();
                 }
             }
